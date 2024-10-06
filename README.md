5618858605e1625883f60861ab697db773ff4ad1
WhosMyName <chikangwu95@gmail.com>
2024-10-06 16:34:59 +0200

    improved addon to search for actordelta, README, and version increase

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # FoundryVTT-FindActorTokenByID
+
 Finds a provided Actor/Token ID
+
+Its a pretty verbose and hacky debugging tool.
+
+### Installation
+
+Install by adding it via the Add-on Modules -> Install Module -> "Manifest URL" -> add the following url -> Install  
+`https://raw.githubusercontent.com/WhosMyName/FoundryVTT-FindActorTokenByID/refs/heads/main/module.json`
+
+Wait for the download to finsih and open your module directory at {userdir}/FoundryVTT/Data/modules/findactortokenbyid.  
+Edit the file called `findtoken.js` and edit the id in Line 4: searchID and replace the id with your id. Save and exit.
+
+Start your World and activate the Module in  
+Game Settings -> Manage Modules -> FindActorTokenByID -> check the mark , save the module settings and reload.
+
+## Usage
+
+The addon searches for specified id in the actor, actordelta and token objects in their respective id and uuid variables.
+To use the addon press F12 in your world and hit space to pause/unpause the game, executing the addon.
+
+Repeatedly edit/exchange/save the ids and reload FoundryVTT using F5 to repeat this for multiple IDs.
+
+Disable the Addon in the Module Settings after usage.
--- findtoken.js
+++ findtoken.js
@@ -1,7 +1,11 @@
 /* displays a short summary of actors in the game and displays a specific actor if the id matches */
 
 Hooks.on("pauseGame", function () {
-  let searchID = "FcBP53ETJwSFkePC"; // enter id here
+  let searchID = "F4kcm3xSTLZLrk5q"; // enter id here
+  console.log(
+    "---------------------------------------------------------- FIND ACTOR(DELTA)/TOKEN BY ID --------------------------------------------------------------------"
+  );
+  console.log("Searching for id: " + searchID);
   // let actors = game.actors;
   // console.log(game);
   // console.log(game.canvas.scene.tokens);
@@ -15,14 +19,36 @@ Hooks.on("pauseGame", function () {
     if (actor.id == searchID) {
       console.log(actor);
     }
+    if (actor.uuid.includes(searchID)) {
+      console.log(actor);
+    }
   });
 
   if (game.canvas.scene !== null && game.canvas.scene.tokens !== null) {
     game.canvas.scene.tokens.forEach((token) => {
-      console.log('Token "' + token.name + '" has id: ' + token.id);
+      console.log(
+        'Token "' +
+          token.name +
+          '" has id: \n' +
+          token.id +
+          "\nActorDelta: \n" +
+          token.delta.id
+      );
       if (token.id == searchID) {
+        console.log(token);
+      }
+      if (token.uuid.includes(searchID)) {
+        console.log(token);
+      }
+      if (token.delta !== null && token.delta.id == searchID) {
+        console.log(token);
+      }
+      if (token.delta.uuid.includes(searchID)) {
         console.log(token);
       }
     });
   }
+  console.log(
+    "---------------------------------------------------------- FIND ACTOR(DELTA)/TOKEN BY ID --------------------------------------------------------------------"
+  );
 });
